b536982db92c50467e3c5e603d3a29641e0e4013
Toan Dinh <taplamtiktok686@gmail.com>
2025-01-21 10:25:52 +0700

    add  Bai9 khai niem Template Engine

--- Bai9 khai niem Template Engine/index.js
+++ Bai9 khai niem Template Engine/index.js
@@ -5,6 +5,10 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.engine('handlebars', handlebars());
+app.set('view engine, handlebars');
+
+console.log(__dirname)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
